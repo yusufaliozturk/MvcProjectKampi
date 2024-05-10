96b323f8b17e3340538da0a87b103b0666a24650
Yusuf Ali Öztürk <ozturk.ysfali@gmail.com>
2024-05-10 20:39:43 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Concrete.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+        public List<Category> GetAll()
+        {
+            return repo.list();
+
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repository/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Abstact;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repository
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        DbSet<Category> _object;
+        public void delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public Category Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> list()
+        {
+            return _object.ToList();
+        }
+
+        public List<Category> list(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(Category p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
